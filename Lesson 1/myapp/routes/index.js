39f58498866106f3387494a7b2251d9f3586fb3f
Robert Thompson <robert.thompson@dudleycol.ac.uk>
2024-09-08 12:44:53 +0100

    Main content for lesson

--- Lesson 1/myapp/routes/index.js
+++ Lesson 1/myapp/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+//Get the package for mongodb
+const { MongoClient } = require("mongodb");
+
+//set the url for the connection to the database
+const uri = "mongodb://localhost:27017/";
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  //connect to the database
+  const client = new MongoClient(uri);
+
+  //select the database on the server
+  const database = client.db("forum");
+  //select the collection to use within the database
+  const collection = database.collection("posts");
+
+  //find will get the whole contents of the database
+  //.toArray() converts this to an array so we can use it easily
+  const posts = await collection.find().toArray();
+
+  //return a repsonse with the index page
+  //pass it through the posts that we found
+  res.render('index', { posts: posts});
 });
 
 module.exports = router;
