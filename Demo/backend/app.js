de2ccae6843999beea03389c00384c813e366968
Robert Thompson <robert.thompson@dudleycol.ac.uk>
2024-10-13 11:27:50 +0100

    Update app.js

--- Demo/backend/app.js
+++ Demo/backend/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sitesRouter = require('./routes/sites');
 
 var app = express();
 app.use(cors())
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sites',sitesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
